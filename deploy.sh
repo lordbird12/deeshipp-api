b28e6dd615a4b932d1b149b880bf6d71f49ad35d
Anon Thamcharoen <anonnon3952@gmail.com>
2023-06-15 17:39:34 +0700

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Update codebase
+git fetch origin main
+git reset --hard origin/main
+
+# Install dependencies based on lock file
+composer install -q --no-ansi --no-interaction --no-scripts --no-progress --prefer-dist
+
+# Migrate database
+php artisan migrate --force
+
+# Clear cache
+php artisan optimize
+
+# Permission
+chmod -R 777 storage bootstrap/cache
+
+# Reload PHP to update opcache
+echo "" | sudo -S service php8.1-fpm reload
+
+echo "Application deployed!"
